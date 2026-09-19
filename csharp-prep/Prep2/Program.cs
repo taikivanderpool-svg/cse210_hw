309a15d9613bf15bea6c711d82b507ce0505f86e
taikivanderpool-svg <taiki.vanderpool@gmail.com>
2026-09-19 15:12:03 -0600

    week01 prove part 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage grade? "); 
+        string stringGrade = Console.ReadLine(); 
+        float gradePercentage = float.Parse(stringGrade);
+        string letter = "";
+
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        float remainder = gradePercentage % 10; 
+        string plusOrMinus = "";
+
+        if (!(gradePercentage >=90 || gradePercentage < 70))
+        {
+            if (remainder >= 7) 
+                {
+                    plusOrMinus = "+";
+                }
+            else if (remainder < 3)
+            {
+                plusOrMinus = "-";
+            }
+        }
+
+        if (gradePercentage >=70)
+        {
+            Console.WriteLine($"Congradulations, at {gradePercentage}% you have passed with a {letter}{plusOrMinus}.");
+        }
+        else
+        {
+            Console.WriteLine($"Unfortunately at {gradePercentage} you have not passed with a {letter}{plusOrMinus}, but you can do it, try again next time.");
+        }
     }
 }
