89ca38fe4a9635fb8ba3f0d91245ebfe1b5b14ee
taikivanderpool-svg <taiki.vanderpool@gmail.com>
2026-09-25 15:21:32 -0600

    C# prep 03

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int magicGuess = -1;
+        
+        int guesses = 0;
+
+        while (!(magicGuess == magicNumber))
+        {
+
+        Console.Write("Guess the magic number? ");
+        string guess = Console.ReadLine();
+        magicGuess = int.Parse(guess);
+        
+        if (magicGuess > magicNumber)
+        {
+            Console.WriteLine("Lower ");
+            guesses ++;
+        }
+        else if (magicGuess < magicNumber)
+        {
+            Console.WriteLine("higher");
+            guesses ++;
+        }
+        else
+        {
+            guesses ++;
+            Console.WriteLine($"Congradulations, you guessed it in {guesses} attempts.");
+        }
+        }
+
+    }   
 }
