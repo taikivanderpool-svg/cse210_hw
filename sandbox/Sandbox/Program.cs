fe3d3b4e1b7b1d183011601e4b9112ac1ae82724
taikivanderpool-svg <taiki.vanderpool@gmail.com>
2026-09-25 14:48:05 -0600

    testing in sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        
+        
+        static float tenth(int amount)
+        {
+            return amount / 10;
+        }
+
+        float tithe = tenth(90);
+        Console.WriteLine(tithe);
+
+
+
     }
 }
